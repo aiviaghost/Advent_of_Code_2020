fbffd80046af50c56f3c71c4d7a237c90ceea22f
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2020-12-02 10:09:37 +0100

    Maybe I should read the description more thoroughly

--- Day_2/solve2.py
+++ Day_2/solve2.py
@@ -8,11 +8,5 @@ while (line := input()) != "":
 			tot += 1
 		elif passw[i - 1] != c[: -1] and passw[j - 1] == c[: - 1]:
 			tot += 1
-	elif 0 <= i - 1 < len(passw):
-		if passw[i - 1] == c[: -1]:
-			tot += 1
-	elif 0 <= j - 1 < len(passw):
-		if passw[j - 1] == c[: -1]:
-			tot += 1
 
 print(tot)
