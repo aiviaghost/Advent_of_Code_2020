f83919fd5ecd0cd546f0a92847d885bf53c3e2a8
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2020-12-11 13:41:28 +0100

    Very clean.

---
+++ Day_11/solve1.py
@@ -0,0 +1,39 @@
+grid = []
+while (line := input()) != "":
+	grid.append(line)
+
+w, h = len(grid[0]), len(grid)
+print(w, h)
+def is_inside(x, y):
+	return 0 <= x < w and 0 <= y < h
+
+has_changed = True
+while has_changed:
+	changed = False
+	next_grid = [[' '] * w for _ in range(h)]
+	for i in range(h):
+		for j in range(w):
+			neighbours = 0
+			for k in range(-1, 2):
+				for l in range(-1, 2):
+					if is_inside(j + l, i + k) and (k != 0 or l != 0) and grid[i + k][j + l] == "#":
+						neighbours += 1
+
+			if grid[i][j] == 'L' and neighbours == 0:
+				next_grid[i][j] = '#'
+				changed = True
+			elif grid[i][j] == '#' and neighbours >= 4:
+				next_grid[i][j] = 'L'
+				changed = True
+			else:
+				next_grid[i][j] = grid[i][j]
+	has_changed = changed
+	grid = next_grid
+
+count = 0
+for i in range(h):
+	for j in range(w):
+		if grid[i][j] == '#':
+			count += 1
+
+print(count)
